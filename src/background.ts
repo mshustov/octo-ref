e04191b744acee80963d2f69fc56fceeed799f46
restrry <restrry@gmail.com>
2017-01-15 17:36:14 +0100

    remove old artefact

--- src/background.ts
+++ src/background.ts
@@ -1,17 +1,10 @@
 import Server from './lib/server';
 const server = new Server();
-// cache is cleared on every browser restart due to manifest settings
-const tabIdToUrl = {};
 
 chrome.runtime.onMessage.addListener((request, sender, callback) => {
     const { url, content } = request.data; // NOTE: sender.url !== window.location.url
 
     switch(request.cmd){
-        case 'register':
-            // we don't want to request 'tab' permissions
-            tabIdToUrl[sender.tab.id] = url;
-            callback('registered');
-            break;
 
         case 'definition':
             const {line, character} = request.data.end;
@@ -23,8 +16,3 @@ chrome.runtime.onMessage.addListener((request, sender, callback) => {
             throw new Error('Unknown type');
     }
 });
-
-chrome.tabs.onRemoved.addListener(function(tabId){
-    const filename = tabIdToUrl[tabId];
-    delete tabIdToUrl[tabId];
-})
